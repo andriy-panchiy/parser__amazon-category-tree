3cc86e80de2417483799a80e3a8f0d6f8359fa89
Andrew Crook <7helfs@gmail.com>
2023-06-04 21:59:42 +0300

    fix bugs when parsing was down

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 const puppeteer = require("puppeteer");
 const fs = require("fs");
 
-const rootTree = {};
 const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 const fetchDelay = 1000;
+
+const parseJSON = (json) => {
+  try {
+    return JSON.parse(json);
+  } catch (error) {
+    return null;
+  }
+};
+
+const rootTreeFile = fs.readFileSync("./tree.json", "utf8");
+const rootTreeJSON = parseJSON(rootTreeFile);
 
 async function parseAmazonLinks() {
   const defaultViewport = { width: 1920, height: 1080 };
@@ -57,19 +67,9 @@ async function parseAmazonLinks() {
 
   await delay(1000);
 
-  async function parseLinks(parentCategory) {
-    const categoryTitleElem = await page.$('div:has(> div[role="treeitem"] + div[role="group"]) > div[role="treeitem"]');
-    const categoryGroupElem = await page.$('div:has(> div[role="treeitem"] + div[role="group"]) > div[role="treeitem"] + div[role="group"] > div[role="treeitem"]');
-    const categoryTitle = await page.evaluate((el) => el.textContent, categoryTitleElem);
-
-    if (!parentCategory || !categoryTitle || (parentCategory && categoryTitle && categoryTitle !== parentCategory)) {
-      return {};
-    }
-
-    const category = await categoryGroupElem.$eval("a", (a) => a.textContent.trim());
-    const url = await categoryGroupElem.$eval("a", (a) => a.href);
-
-    const subTree = await page.$$eval('div[role="treeitem"] + div[role="group"] > div[role="treeitem"]', (elements) => {
+  const rootTree =
+    rootTreeJSON ||
+    (await page.$$eval('div[role="treeitem"] + div[role="group"] > div[role="treeitem"]', (elements) => {
       return elements.reduce((acc, el) => {
         acc[el.textContent.trim()] = {
           url: el.querySelector("a").href,
@@ -77,9 +77,38 @@ async function parseAmazonLinks() {
         };
         return acc;
       }, {});
+    }));
+
+  async function parseLinks(parentCategory) {
+    const categoryTitleElem = await page.$('div:has(> div[role="treeitem"] + div[role="group"]) > div[role="treeitem"]');
+    const categoryGroupElem = await page.$('div:has(> div[role="treeitem"] + div[role="group"]) > div[role="treeitem"] + div[role="group"] > div[role="treeitem"]');
+    const categoryTitle = await page.evaluate((el) => el?.textContent, categoryTitleElem);
+
+    if (!parentCategory || !categoryTitle || categoryTitle !== parentCategory) return {};
+
+    let category;
+    let url;
+
+    try {
+      category = await categoryGroupElem.$eval("a", (a) => a?.textContent?.trim());
+      url = await categoryGroupElem.$eval("a", (a) => a?.href);
+    } catch (error) {
+      console.log(parentCategory, categoryTitle);
+      return {};
+    }
+
+    const subTree = await page.$$eval('div[role="treeitem"] + div[role="group"] > div[role="treeitem"]', (elements) => {
+      return elements.reduce((acc, el) => {
+        acc[el.textContent.trim()] = {
+          url: el.querySelector("a")?.href,
+          tree: {},
+        };
+        return acc;
+      }, {});
     });
 
     for (const [category, value] of Object.entries(subTree)) {
+      if (!value.url) continue;
       await page.goto(value.url);
       await delay(fetchDelay);
 
@@ -95,17 +124,55 @@ async function parseAmazonLinks() {
     };
   }
 
-  const rootTree = await page.$$eval('div[role="treeitem"] + div[role="group"] > div[role="treeitem"]', (elements) => {
-    return elements.reduce((acc, el) => {
-      acc[el.textContent.trim()] = {
-        url: el.querySelector("a").href,
-        tree: {},
-      };
-      return acc;
-    }, {});
-  });
-
   for (const [category, value] of Object.entries(rootTree)) {
+    switch (category) {
+      case "Amazon Devices & Accessories":
+      case "Amazon Renewed":
+      case "Appliances":
+      case "Apps & Games":
+      case "Arts, Crafts & Sewing":
+      case "Audible Books & Originals":
+      case "Automotive":
+      case "Baby":
+      case "Beauty & Personal Care":
+      case "Books":
+      case "Camera & Photo Products":
+      case "CDs & Vinyl":
+      case "Cell Phones & Accessories":
+      case "Climate Pledge Friendly":
+      case "Clothing, Shoes & Jewelry":
+      case "Collectible Coins":
+      case "Computers & Accessories":
+      case "Digital Educational Resources":
+      case "Digital Music":
+      case "Electronics":
+      case "Entertainment Collectibles":
+      case "Gift Cards":
+      case "Grocery & Gourmet Food":
+      case "Handmade Products":
+      case "Health & Household":
+      case "Home & Kitchen":
+      case "Industrial & Scientific":
+      case "Kindle Store":
+      case "Kitchen & Dining":
+      case "Magazine Subscriptions":
+      case "Movies & TV":
+      case "Musical Instruments":
+      case "Office Products":
+      case "Patio, Lawn & Garden":
+      case "Pet Supplies":
+      case "Software":
+      case "Sports & Outdoors":
+      case "Sports Collectibles":
+      case "Tools & Home Improvement":
+      case "Toys & Games":
+      case "Unique Finds":
+      case "Video Games":
+        continue;
+    }
+
+    if (!value.url) continue;
+
     await page.goto(value.url);
     await delay(fetchDelay);
 
@@ -117,10 +184,4 @@ async function parseAmazonLinks() {
   return rootTree;
 }
 
-parseAmazonLinks()
-  .then((tree) => {
-    fs.writeFileSync("./tree.json", JSON.stringify(tree, null, 2));
-  })
-  .catch((error) => {
-    console.error("Error:", error);
-  });
+parseAmazonLinks();
